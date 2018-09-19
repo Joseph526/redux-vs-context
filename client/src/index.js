987996ad702e293766d3af3ba924002fc084b7be
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-18 20:07:48 -0400

    Add redux store provider to App render

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create the store using the reducers info.
+// Reducers are the building blocks of a Redux Store.
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
