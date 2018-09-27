b52f249589a59e549b9bffa0bf4356a1271a2102
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-27 15:22:04 -0400

    Convert App from class to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import actions from './actions';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    const { food, searchTerm, searchTermChanged } = this.props;
-
-    this.food = food;
-    this.searchTerm = searchTerm;
-    this.searchTermChanged = searchTermChanged;
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="search">
-          <input
-            type="text"
-            name="search"
-            placeholder="Search"
-            value={this.searchTerm}
-            onChange={e => this.searchTermChanged(e.target.value)}
-          />
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Origin</th>
-              <th>Continent</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.food.map(theFood => (
-              <tr key={theFood.name}>
-                <td>{theFood.name}</td>
-                <td>{theFood.origin}</td>
-                <td>{theFood.continent}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+function App({ food, searchTerm, searchTermChanged }) {
+  return (
+    <div>
+      <div className="search">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={e => searchTermChanged(e.target.value)}
+        />
       </div>
-    );
-  }
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Origin</th>
+            <th>Continent</th>
+          </tr>
+        </thead>
+        <tbody>
+          {food.map(theFood => (
+            <tr key={theFood.name}>
+              <td>{theFood.name}</td>
+              <td>{theFood.origin}</td>
+              <td>{theFood.continent}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default connect(store => store, actions)(App);
