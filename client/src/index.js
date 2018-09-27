100df61c08d8d20a6f35d3817c39b65e7e9e5e8e
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-27 16:27:32 -0400

    Convert App render using Context Provider and Consumer

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import Provider from './providers';
+import Consumer from './consumer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// Create the store using the reducers info.
-// Reducers are the building blocks of a Redux Store.
-const store = createStore(reducers);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
+    <Provider>
+        <Consumer>
+            <App />
+        </Consumer>
     </Provider>,
     document.getElementById('root')
 );
