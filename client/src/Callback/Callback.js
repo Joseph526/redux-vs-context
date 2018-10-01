91cdfa13eeb1b7f3eb71db9396b9771e107110aa
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-10-01 17:14:20 -0400

    Change history.push() to history.replace()

--- client/src/Callback/Callback.js
+++ client/src/Callback/Callback.js
@@ -3,7 +3,7 @@ import { withRouter } from "react-router";
 
 function Callback(props) {
     props.auth.handleAuthentication().then(() => {
-        props.history.push("/");
+        props.history.replace("/");
     });
 
     return (
